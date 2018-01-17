451f2632eb7d420f69d4a4a6d00d16496bc9aa8e
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-01-17 18:10:45 -0200

    ajustes para implementar o processo de segurança de pessoa service

---
+++ spring-pessoa/src/main/java/br/com/springpessoa/security/UnauthorizedHandler.java
@@ -0,0 +1,43 @@
+package br.com.springpessoa.security;
+
+import br.com.springexception.throwables.security.SpringBootSecurityUnauthorizedException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * O componente é invocado caso se tente acessar qualquer recurso sem credenciais
+ */
+@Component
+public class UnauthorizedHandler implements AuthenticationEntryPoint, Serializable {
+
+    private final String urlLogin;
+
+    public UnauthorizedHandler(@Value("${springboot.security.jwt.controller.loginEndPoint}") final String urlLogin) {
+        this.urlLogin = urlLogin;
+    }
+
+    @Override
+    public void commence(final HttpServletRequest request,
+                         final HttpServletResponse response,
+                         final AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter()
+                .print(new SpringBootSecurityUnauthorizedException("Unauthorized!")
+                        .addDetails("urlLogin", request
+                                .getRequestURL()
+                                .toString()
+                                .replace(request.getRequestURI(), "") + urlLogin
+                        ).toJson());
+
+    }
+}
