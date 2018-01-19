962e0888e163a80e2ce06aca7dc031c09b2adcf6
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-01-19 08:40:30 -0200

    adicionado o modulo de rest-util

---
+++ spring-rest-util/src/main/java/br/com/springrestutil/SpringRestUtilApplication.java
@@ -0,0 +1,12 @@
+package br.com.springrestutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestUtilApplication.class, args);
+    }
+}
