8c75988642a731daf9e8cbefe6ac0fdebefd94cf
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-01-22 15:36:06 -0200

    adicionado o serviço de endereço

---
+++ spring-endereco/src/main/java/br/com/springendereco/SpringEnderecoApplication.java
@@ -0,0 +1,12 @@
+package br.com.springendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEnderecoApplication.class, args);
+	}
+}
