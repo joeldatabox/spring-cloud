05ea340d31638462c769b0f38b60f9e9564920f8
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-01-24 13:58:22 -0200

    configuração do ribbon

--- spring-endereco/src/main/java/br/com/springendereco/controller/EnderecoController.java
+++ spring-endereco/src/main/java/br/com/springendereco/controller/EnderecoController.java
@@ -1,6 +1,9 @@
 package br.com.springendereco.controller;
 
 import br.com.springmodel.model.Endereco;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +22,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/enderecos")
 public class EnderecoController {
+    private static final Log log = LogFactory.getLog(EnderecoController.class);
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity findAll(@PathVariable("id") String id, @RequestParam Map<String, Object> allRequestParams, HttpServletResponse response) {
+        log.info("respondendo requisicao na porta " + port);
         return ResponseEntity.ok(new Endereco(id, "Endereco " + id));
     }
 }
